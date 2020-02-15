d9e2230a8ca7a4c09e950abfdfbd9b172d424632
Blake Rieschick <blake@awnix.com>
2020-02-14 18:51:06 -0600

    alg stable

--- stable_matching_d819r197.py
+++ stable_matching_d819r197.py
@@ -47,22 +47,22 @@ class PersonPairing:
             self.freeManQueue.append(man)
 
     def showInterestMatrices(self):
-        print("Male Interests: ")
+        #print("Male Interests: ")
         for male in self.maleInterestMatrix:
             print(male.interestRanking)
         print("\n", end = '')
-        print("Female Interests: ")
+        #print("Female Interests: ")
         for woman in self.femaleInterestMatrix:
             print(woman.interestRanking)
 
     def showPairs(self):
-        print("Resulting Pairs of Couples: ")
+        print("\nResulting Pairs of Couples: ")
         print("[", end = '')
         for male in self.maleInterestMatrix:
             if male.engagedTo != None:
-                print("(" + str(male.id) + "," + str(male.engagedTo.id) + ") ", end = '')
+                print("(" + str(male.id+1) + "," + str(male.engagedTo.id+1) + ") ", end = '')
             else:
-                print("(Male " + str(male.id) +" is not engaged to anyone)")
+                print("(Male " + str(male.id+1) +" is not engaged to anyone)")
         print("]")
 
     def stableMatch(self):
@@ -70,41 +70,59 @@ class PersonPairing:
         while(len(self.freeManQueue) != 0):
             #Choose Such a Man
             man = self.freeManQueue.pop(0)
-            print("Running with Man: " + str(man.id))
+            #print("Running with Man: " + str(man.id + 1))
             if man.engagedTo == None and len(man.proposedTo) != self.size:
                 #Let W be the highest ranked woman for the given man in mans preference list to whom he hasnt proposed
                 for interest in man.interestRanking:
+                    #print("Looking at woman: " + str(interest))
                     #Define current woman
-                    woman = self.femaleInterestMatrix.pop(interest)
-                    self.femaleInterestMatrix.insert(interest, woman)
+                    woman = self.femaleInterestMatrix.pop(interest-1)
+                    self.femaleInterestMatrix.insert(interest-1, woman)
                     #W is free for marriage
                     if man.proposedTo == [] or woman not in man.proposedTo:
                         #W is not currently Engaged
                         if woman.engagedTo == None:
+                            #print("Engaging [M,W]: " + str([[man.id+1],[interest]]))
                             #Engage the Man and Woman
                             man.engagedTo = woman
+                            woman.engagedTo = man
+
                             #Add woman to list of proposed woman for the given man
                             man.proposedTo.append(woman)
+                            break
                         #W is currently engaged to M'
                         else:
+                            #print("Attemptted to engage married woman")
                             #W prefers M' to M
-                            if interest.interestRanking.index(man) < interest.interestRanking.index(interest.engagedTo):
+                            #print("Interest Val, New man: " + str(man.id+1) + " and Old Man: " + str(woman.engagedTo.id+1))
+                            #print("Index Val, New man: " + str(woman.interestRanking.index(man.id+1)) + " and Old Man: " + str(woman.interestRanking.index(woman.engagedTo.id+1)))
+
+                            if woman.interestRanking.index(man.id+1) > woman.interestRanking.index(woman.engagedTo.id+1):
+                                #print("New man pushed back onto queue")
                                 #Then Push M back on to the queue
+                                man.proposedTo.append(woman)
                                 self.freeManQueue.append(man)
+                                break
                             #W prefers M to M'
                             else:
-                                notM = interest.engagedTo
+                                notM = woman.engagedTo
                                 #M and W become engaged
-                                man.engagedTo = interest
+                                #print("Engaging New [M,W]: " + str([[man.id+1],[woman.id+1]]))
+                                man.engagedTo = woman
+                                woman.engagedTo = man
+                                man.proposedTo.append(woman)
+
                                 #M' is Pushed back on Queue
+                                notM.engagedTo = None
                                 self.freeManQueue.append(notM)
+                                break
 
 
 def main():
     personPair = PersonPairing(sys.argv[1])
     personPair.showInterestMatrices()
     personPair.stableMatch()
-    #personPair.showPairs()
+    personPair.showPairs()
 
 if __name__ == "__main__":
     main()
